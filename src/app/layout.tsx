55e9303841352e744b52ad9e97cccac05061e3f9
Ashkan <ashkan.mohammed98@gmail.com>
2025-04-16 10:53:35 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, Viewport } from "next"
+import type { Metadata } from "next"
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
