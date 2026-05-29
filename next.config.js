444d6a01995719cd191d8266d277c32a8d8eebe8
Mohamed Ashkan <65010721+Mohammedashkan@users.noreply.github.com>
2026-05-29 16:17:09 +0530

    Add output configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   transpilePackages: ['debug', 'supports-color'],
   experimental: {
     serverComponentsExternalPackages: ['debug', 'supports-color']
